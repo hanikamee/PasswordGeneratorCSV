d97ff64edecc1e95bdc8f241216ac83f8d145a5c
hanikamee <hani.young13@yahoo.com>
2022-08-23 16:33:30 -0500

    Updated the Input Validation

--- src/PasswordGeneratorCSV.py
+++ src/PasswordGeneratorCSV.py
@@ -14,11 +14,12 @@ class Password:
         
     def generate(self) -> str:
         
-        password_length = int(input("Please enter the desired length for your password: "))
+        password_length = int(input("Please enter the desired length for your password (must be at least 5 characters): "))
 
-        # input validation
-        while type(password_length) != int:
-            password_length = int(input("Please enter the desired length for your password: "))
+        # input validation 
+        while password_length < 5:
+            print("Your password length is too short")
+            password_length = int(input("Please enter the desired length for your password (must be at least 5 characters): "))
 
         # string.printable includes lowercase and uppercase letters, numbers, and printable symbols
         password_characters = list(string.printable.strip(" \t\n\r\x0b\x0c"))
@@ -61,7 +62,7 @@ def main():
         password.generate() # Calling the generate function
         print(f'The newly generated password for {name} is {password.password}')
         
-        if (save_password := input("Would you like the password and username to be saved? (y/n): ").lower()) == 'y':
+        if (save_password := input("Would you like the password and username to be saved? (y/exit): ").lower()) == 'y':
             if password.store(password.password, name) == 0:
                 print("Your password was successfully saved!")
             else:
